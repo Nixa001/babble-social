d311b7d0a0172918bc8e91600d912c4e3f94b0e4
mamdrame <mamourkifa20@gmail.com>
2024-03-06 14:53:09 +0000

    Corriger une faute de frappe dans le modèle Confirm

--- backend/models/confirm_model.go
+++ backend/models/confirm_model.go
@@ -3,5 +3,5 @@ package models
 type Confirm struct {
 	UserIDAsker int `json:"user_id"`
 	UserIDAsked int `json:"user_id_answerer"`
-	GroupId     int `json:"groupId`
+	GroupId     int `json:"groupId"`
 }
