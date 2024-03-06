c4f7f0d7269b42acfed64a81be99b3159c5381f1
mamdrame <mamourkifa20@gmail.com>
2024-03-06 13:34:37 +0000

    Ajouter le modèle de session avec les champs Id, ExpirationDate et Data

--- backend/models/session.model.go
+++ backend/models/session.model.go
@@ -1,1 +1,9 @@
 package models
+
+import "github.com/gofrs/uuid/v5"
+
+type Session struct {
+	Id             uuid.UUID `json:"id"`
+	ExpirationDate string    `json:"expirationDate"`
+	Data           string    `json:"data"`
+}
