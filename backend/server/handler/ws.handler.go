1452781c9c9efa6c875263e24ec2d48f2669eddb
mamdrame <mamourkifa20@gmail.com>
2024-03-08 14:46:50 +0000

    Ajouter la vérification du jeton d'authentification

---
+++ backend/server/handler/ws.handler.go
@@ -0,0 +1,16 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func WSHandler(w http.ResponseWriter, r *http.Request) {
+
+}
--- backend/server/service/auth.service.go
+++ backend/server/service/auth.service.go
@@ -4,6 +4,8 @@ import (
 	"backend/models"
 	r "backend/server/repositories"
 	"fmt"
+	"net/http"
+	"strings"
 
 	"golang.org/x/crypto/bcrypt"
 )
@@ -35,11 +37,19 @@ func (a *AuthService) CreateUser(user *models.User) error {
 func (a *AuthService) CheckCredentials(email, password string) (*models.User, error) {
 	user, err := a.UserRepo.GetUserByEmail(email)
 	if err != nil {
-		return &models.User{}, fmt.Errorf("Invalid Credentials")
+		return &models.User{}, fmt.Errorf("invalid credentials")
 	}
 	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
 	if err != nil {
-		return &models.User{}, fmt.Errorf("Invalid Credentials")
+		return &models.User{}, fmt.Errorf("invalid credentials")
 	}
 	return user, nil
+}
+
+func (a *AuthService) VerifyToken(r *http.Request) (session models.Session, err error) {
+	token := r.Header.Get("Authorization")
+	if strings.TrimSpace(token) == "" {
+		return models.Session{}, fmt.Errorf("no token provided")
+	}
+	return session, nil
 }
