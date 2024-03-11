fffdd883016f105c88cf40a183a7eb2402c1db7d
mass <massaw6@gmail.com>
2024-03-11 21:13:07 +0000

    front-end: ws connection added

--- frontend/app/ui/home/page.js
+++ frontend/app/ui/home/page.js
@@ -1,20 +1,45 @@
-"use client"
-import React from "react";
+"use client";
+import React, { useState, useEffect } from "react";
 import { postForm } from "./postForm";
 import AddPost from "./displayPost";
 
 const HomePage = () => {
+  const props = {
+    data: "This is a test",
+    title: "Test Title",
+  };
+  const [data, setData] = useState(props.data);
+  const [ws, setWS] = useState(null);
+  useEffect(() => {
+    const newWS = new WebSocket("ws://localhost:8080/socket");
+    newWS.onerror = (err) => console.error(err);
+    newWS.onopen = () => {
+      setWS(newWS);
+      // send data to server when connected
+      newWS.send(JSON.stringify({ type: "GET", request: "get10post" }));
+    };
+    newWS.onmessage = (msg) => {
+      setData(JSON.parse(msg.data));
+      console.log(`data is here => : ${data}`);
+    };
+  }, []);
   return (
     <div className=" md:w-[400px] lg:w-[650px] xl:w-[800px] 2xl:w-[1000px] w-screen">
-      <div className="flex justify-center mb-4">
-        {postForm()}
-      </div>
+      <div className="flex justify-center mb-4">{postForm()}</div>
       <div className="post_div_top flex flex-col items-center">
-        <AddPost postData={postData} onLikeClick={onLikeClick} onDislikeClick={onDislikeClick}
-          onCommentClick={onCommentClick} onProfileClick={onProfileClick}
+        <AddPost
+          postData={postData}
+          onLikeClick={onLikeClick}
+          onDislikeClick={onDislikeClick}
+          onCommentClick={onCommentClick}
+          onProfileClick={onProfileClick}
         />
-        <AddPost postData={postData2} onLikeClick={onLikeClick} onDislikeClick={onDislikeClick}
-          onCommentClick={onCommentClick} onProfileClick={onProfileClick}
+        <AddPost
+          postData={postData2}
+          onLikeClick={onLikeClick}
+          onDislikeClick={onDislikeClick}
+          onCommentClick={onCommentClick}
+          onProfileClick={onProfileClick}
         />
       </div>
     </div>
@@ -32,7 +57,7 @@ const postData = {
   likesCount: 190,
   dislikesCount: 20,
   commentsCount: 3,
-}
+};
 const postData2 = {
   profilePicture: "/assets/profilibg.jpg",
   userName: "Maurice Dassylva",
@@ -44,26 +69,22 @@ const postData2 = {
   likesCount: 19,
   dislikesCount: 20,
   commentsCount: 3,
-}
+};
 
 export default HomePage;
 
 const onLikeClick = () => {
-  alert('like')
+  alert("like");
 };
 
 const onDislikeClick = () => {
-  alert('dislike')
-
+  alert("dislike");
 };
 
 const onCommentClick = () => {
-  alert('Comment disp')
-
+  alert("Comment disp");
 };
 
 const onProfileClick = () => {
-  alert('profile disp')
-
+  alert("profile disp");
 };
-
