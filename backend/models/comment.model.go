06ca686b17fdd07b1d53144e10ebc37c4cfd65a9
mass <massaw6@gmail.com>
2024-03-14 00:15:14 +0000

    backend: modals updated

--- backend/models/comment.model.go
+++ backend/models/comment.model.go
@@ -4,12 +4,15 @@ type Comment struct {
 	ID      int
 	Date    string
 	Content string `json:"content"`
-	Post_id int    `json:"post_id"`
-	User_id int    `json:"user_id"`
+	Post_id int    `json:"postID"`
+	User_id int    `json:"userID"`
 	Media   string `json:"media"`
 }
 
 type DataComment []struct {
+	FullName      string
+	Username      string
+	Avatar        string
 	Data          []Comment
 	Comment_react [2]int
 }
--- backend/models/post.model.go
+++ backend/models/post.model.go
@@ -3,15 +3,19 @@ package models
 type Post struct {
 	ID       int
 	Date     string
-	Content  string
-	Media    string
-	User_id  int
-	Group_id int
-	Privacy  string
+	Content  string   `json:"content"`
+	Media    string   `json:"media"`
+	User_id  int      `json:"userID"`
+	Group_id int      `json:"groupID"`
+	Privacy  string   `json:"privacy"`
+	Viewers  []string `json:"viewers"`
 }
 
 type DataPost []struct {
+	Avatar        string
 	Data          []Post
+	FullName      string
+	Username      string
 	comments      DataComment
 	Post_reaction [2]int
 }
