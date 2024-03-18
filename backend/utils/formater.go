7225bfecdf205cdde95d8a4b2fde8f5873eb23f8
mass <massaw6@gmail.com>
2024-03-18 15:32:55 +0000

    backend: new utils func added (date generator + decoder + encoder (data insertion))

---
+++ backend/utils/formater.go
@@ -0,0 +1,17 @@
+package utils
+
+import "time"
+
+func Time() (string, string) {
+	current := time.Now()
+	state := "am"
+
+	// check wether the time is past or after morning
+	if current.Hour() >= 12 {
+		state = "pm"
+	}
+
+	date := time.Now().Format("Jan 2, 2006")
+	hour := time.Now().Format("03:04" + " " + state)
+	return date, hour
+}
