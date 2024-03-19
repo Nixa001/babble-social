724f93297c82a83653f5102631172f4af7f249cf
mass <massaw6@gmail.com>
2024-03-19 19:04:32 +0000

    backend: getOnePost request done

--- backend/database/getPosts.sql
+++ backend/database/getPosts.sql
@@ -60,4 +60,40 @@ GROUP BY
     p.content,
     p.media,
     p.date,
-    p.user_id
+    p.user_id;
+
+
+-------getonePost
+
+SELECT
+    p.id AS post_id,
+    p.content AS post_content,
+    p.media AS post_media,
+    p.date AS post_date,
+    p.user_id AS  author_id,
+    u.avatar as avatar,
+    u.user_name as username,
+    concat (u.first_name, " ", u.last_name) as full_name,
+    COUNT(DISTINCT c.id) AS comment_count,
+    GROUP_CONCAT(DISTINCT cat.category) AS categories,
+    CASE
+    WHEN p.privacy ="public" THEN "public"
+    WHEN p.privacy ="private" THEN (
+         SELECT GROUP_CONCAT(user_id_follower) 
+            FROM users_followers 
+            WHERE user_id_followed = p.user_id
+           -- OR user_id_follower = p.user_id  
+    )
+    WHEN p.privacy ="almost" THEN (
+        SELECT GROUP_CONCAT(user_id)
+            FROM viewers
+            WHERE post_id = p.id
+        )
+        ELSE NULL
+    END AS isPublic
+FROM
+    posts AS p
+LEFT JOIN comment AS c ON p.id = c.post_id,
+    categories AS cat ON p.id = cat.post_id,
+    users AS u ON u.id =  p.user_id
+WHERE p.id = 1;
