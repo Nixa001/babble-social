2762ea2c2d6a5108b13e6d99d6000207cbc574b4
mthiaw <massaw6@gmail.com>
2024-03-20 12:21:25 +0000

    backend: postHandler endpoint added

--- backend/server/routes/routes.go
+++ backend/server/routes/routes.go
@@ -10,7 +10,7 @@ const (
 	SIGNUP_ENDPOINT = "/auth/signup"
 	SIGNIN_ENDPOINT = "/auth/signin"
 	LOGOUT_ENDPOINT = "/auth/signout"
-	WS_ENDPOINT = "/socket"
+	POST_ENDPOINT = "/post"
 )
 
 func Route() *http.ServeMux {
@@ -19,7 +19,7 @@ func Route() *http.ServeMux {
 	mux.HandleFunc(SIGNUP_ENDPOINT, handler.SignUpHandler)
 	mux.HandleFunc(SIGNIN_ENDPOINT, handler.SignInHandler)
 	mux.HandleFunc(LOGOUT_ENDPOINT, handler.AuthorizeHandler(handler.SignOutHandler))
-	mux.HandleFunc(WS_ENDPOINT, handler.WSHandler)
+	mux.HandleFunc(POST_ENDPOINT, handler.POSTHandler)
 
 	return mux
 }
