0c6ea651a37bb14baf2818bd0e740a2369c05d8c
mamdrame <mamourkifa20@gmail.com>
2024-03-21 04:54:43 +0000

    Corriger le nom du champ "id" en "token" dans le modèle de session

--- backend/models/session.model.go
+++ backend/models/session.model.go
@@ -1,7 +1,7 @@
 package models
 
 type Session struct {
-	Token      string `json:"id"`
+	Token      string `json:"token"`
 	User_id    int    `json:"user_id"`
 	Expiration string `json:"expiration"`
 }
