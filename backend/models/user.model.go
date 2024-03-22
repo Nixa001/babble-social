e2aeb9a3289ade0181a6b83cfdddfbc314250458
mamdrame <mamourkifa20@gmail.com>
2024-03-22 04:48:56 +0000

    Ajouter une structure de données pour les utilisateurs formatés

--- backend/models/user.model.go
+++ backend/models/user.model.go
@@ -13,3 +13,16 @@ type User struct {
 	Password   string `json:"password"`
 	Email      string `json:"email"`
 }
+
+type FormatedUser struct {
+	First_name string `json:"first_name"`
+	Last_name  string `json:"last_name"`
+	User_name  string `json:"user_name"`
+	Gender     string `json:"gender"`
+	User_type  string `json:"user_type"`
+	Birth_date string `json:"birth_date"`
+	Avatar     string `json:"avatar"`
+	About_me   string `json:"about_me"`
+	Password   string `json:"password"`
+	Email      string `json:"email"`
+}
