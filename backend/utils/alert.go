53aad8e4a164916f5986a4f543d90623932ca088
mass <massaw6@gmail.com>
2024-03-22 13:31:12 +0000

    backend: new func to send post in realtime added (ALertPostData)

--- backend/utils/alert.go
+++ backend/utils/alert.go
@@ -16,3 +16,15 @@ func Alert(w http.ResponseWriter, msg models.Errormessage) {
 	w.WriteHeader(msg.StatusCode)
 	json.NewEncoder(w).Encode(response)
 }
+
+func AlertPostData(w http.ResponseWriter, msg models.WResponse) {
+	response := map[string]interface{}{}
+	response["msg"] = msg.Msg
+	response["status"] = msg.StatusCode
+	response["type"] = msg.Type
+	response["display"] = msg.Display
+	response["data"] = msg.Data
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(msg.StatusCode)
+	json.NewEncoder(w).Encode(response)
+}
