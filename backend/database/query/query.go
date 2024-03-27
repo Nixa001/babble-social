603d50557f4a5b9203d3606e8f6a0096fd8fd9e3
mamdrame <mamourkifa20@gmail.com>
2024-03-27 01:46:29 +0000

    Ajouter une instruction d'impression pour afficher la requête SQL générée dans SelectOneFrom()

--- backend/database/query/query.go
+++ backend/database/query/query.go
@@ -67,7 +67,7 @@ func SelectOneFrom(table string, where WhereOption) string {
 
 	whToString := getWhereOptionsString(where)
 	query := fmt.Sprintf("SELECT * FROM %v WHERE %v;", table, whToString)
-
+	fmt.Println(query)
 	return query
 }
 
