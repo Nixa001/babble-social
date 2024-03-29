40004e7fc693e92fe9fd6c9fe20e867146807ac9
mamdrame <mamourkifa20@gmail.com>
2024-03-29 04:16:02 +0000

    Mettre à jour le modèle de session pour utiliser le type time.Time pour l'expiration

--- backend/models/session.model.go
+++ backend/models/session.model.go
@@ -1,7 +1,9 @@
 package models
 
+import "time"
+
 type Session struct {
-	Token      string `json:"token"`
-	User_id    int    `json:"user_id"`
-	Expiration string `json:"expiration"`
+	Token      string    `json:"token"`
+	User_id    int       `json:"user_id"`
+	Expiration time.Time `json:"expiration"`
 }
