1eb18e2a005a3ad39a6cbf0f5df154aa3cc2983f
mamdrame <mamourkifa20@gmail.com>
2024-03-29 04:16:41 +0000

    Mise à jour des routes d'authentification

--- backend/server/routes/routes.go
+++ backend/server/routes/routes.go
@@ -6,18 +6,18 @@ import (
 )
 
 const (
-	HOME_ENDPOINT   = "/"
-	SIGNUP_ENDPOINT = "/auth/signup"
-	SIGNIN_ENDPOINT = "/auth/signin"
-	LOGOUT_ENDPOINT = "/auth/signout"
+	SIGNUP_ENDPOINT     = "/auth/signup"
+	SIGNIN_ENDPOINT     = "/auth/signin"
+	LOGOUT_ENDPOINT     = "/auth/signout"
+	VERIF_SESS_ENDPOINT = "/auth/session"
 )
 
 func Route() *http.ServeMux {
 	mux := http.NewServeMux()
-	mux.HandleFunc(HOME_ENDPOINT, (handler.IndexHandler))
 	mux.HandleFunc(SIGNUP_ENDPOINT, (handler.SignUpHandler))
 	mux.HandleFunc(SIGNIN_ENDPOINT, (handler.SignInHandler))
 	mux.HandleFunc(LOGOUT_ENDPOINT, (handler.AuthorizeMiddleware(handler.SignOutHandler)))
+	mux.HandleFunc(VERIF_SESS_ENDPOINT, (handler.VerifySessionHandler))
 
 	return mux
 }
