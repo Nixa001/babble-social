03dc41e6ba52f1dfc4854648b734cb307981b3d3
mamdrame <mamourkifa20@gmail.com>
2024-04-05 22:04:47 +0000

    Mise à jour de la page de profil pour utiliser la fonction getSession au lieu de useSession

--- frontend/app/home/[profile]/page.js
+++ frontend/app/home/[profile]/page.js
@@ -1,10 +1,12 @@
 "use client";
-import { useSession } from "@/app/api/api.js";
+import { getSession } from "@/app/api/api.js";
 import Profile from "@/app/ui/components/profile.cp/profile.cp.js";
 import { QueryClient, QueryClientProvider } from "react-query";
 const queryClient = new QueryClient();
 const page = () => {
-  const { session, errSess } = useSession();
+  const { session, errSess } = getSession();
+  console.log("session => ", session);
+  console.log("errSess => ", errSess);
   const sessionId = session?.session["user_id"];
   console.log("i got session in group => ", sessionId);
   return (
