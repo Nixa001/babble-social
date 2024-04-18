62d0fc12035885fffc839c37bd9135b26f8cff60
mamdrame <mamourkifa20@gmail.com>
2024-04-18 13:41:23 +0000

    Corrections de chemins de fichiers et de fonctions dans le fichier frontend/app/home/[profile]/page.js

--- frontend/app/home/[profile]/page.js
+++ frontend/app/home/[profile]/page.js
@@ -4,7 +4,8 @@ import Profile from "@/app/ui/components/profile.cp/profile.cp.js";
 import { QueryClient, QueryClientProvider } from "react-query";
 const queryClient = new QueryClient();
 const page = () => {
-  const { session, errSess } = useSession();
+  const token = localStorage.getItem("token");
+  const { session, errSess } = useSession(token);
   const sessionId = session?.session["user_id"];
   const sessionToken = session?.session["token"];
   return (
